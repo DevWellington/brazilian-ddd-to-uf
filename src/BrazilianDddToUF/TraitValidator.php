feb5e452c9c8c1f7abf7fcad26206150a7b20972
Wellington Ribeiro <devwellington@gmail.com>
2014-12-23 20:57:54 -0200

    Update TraitValidator.php

--- src/BrazilianDddToUF/TraitValidator.php
+++ src/BrazilianDddToUF/TraitValidator.php
@@ -7,7 +7,6 @@ trait TraitValidator {
     protected static function validate(array $arrayDDD, $ddd){
         if( !array_key_exists($ddd, $arrayDDD) ) {
             throw new \InvalidArgumentException('DDD inexistente');
-            return false;
         }
 
         return true;
